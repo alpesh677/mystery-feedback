5a477c05aa520f6e0cd2a75d9ea91900c57fb253
alpesh677 <bariaalpesh477@gmail.com>
2024-04-21 22:50:38 +0530

    added user model and signup route

---
+++ src/helpers/SendVerificationEmail.ts
@@ -0,0 +1,29 @@
+import { resend } from "@/lib/resend";
+import VerificationEmail from "../../emails/VerificationEmail";
+import { ApiResponse } from "@/types/ApiResponse";
+
+export async function sendEmailVerificationEmail(
+	username: string,
+	email: string,
+	verifyCode: string,
+): Promise<ApiResponse> {
+	try {
+		await resend.emails.send({
+			from: "Acme <onboarding@resend.dev>",
+			to: email,
+			subject: "Mystry Message Verification Code",
+			react: VerificationEmail({username, otp:verifyCode}),
+		});
+
+        return {
+            success : true,
+            message : "Verification email sent successfully"
+        };
+	} catch (emailError) {
+		console.log("Error sending verification email:", emailError);
+		return {
+			success: false,
+			message: "failed to send a verification email",
+		};
+	}
+}
