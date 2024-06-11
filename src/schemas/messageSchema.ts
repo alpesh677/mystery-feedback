de2ca41dc3be1e57b9d3dfbfba2000bde33e7ac7
alpesh677 <bariaalpesh477@gmail.com>
2024-06-11 14:05:56 +0530

    gitignore is not working

--- src/schemas/messageSchema.ts
+++
@@ -1,8 +0,0 @@
-import { z } from "zod";
-
-export const messageSchema = z.object({
-    content : z
-    .string()
-    .min(10,{message : "Content must be alteast of 10 characters"})
-    .max(300, {message: "message must be no longer than 300 characters"})
-})
